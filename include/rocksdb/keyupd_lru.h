0b25960ed98cc8ddee78058a46cdf461c5f70ff2
linqy71 <2575769386@qq.com>
2022-11-14 02:00:37 +0000

    add cbf/upd/score 3 tables

---
+++ include/rocksdb/keyupd_lru.h
@@ -0,0 +1,81 @@
+#ifndef STORAGE_LEVELDB_DB_KEYUPDLRU_H_
+#define STORAGE_LEVELDB_DB_KEYUPDLRU_H_
+
+#include <string>
+
+#include "rocksdb/sst_score_table.h"
+#include "rocksdb/cache.h"
+#include "rocksdb/slice.h"
+
+
+namespace ROCKSDB_NAMESPACE {
+
+	// value -> void*
+	static void* EncodeValue(uint64_t* v) { return reinterpret_cast<void*>(v); }
+
+	// void* -> value and delete
+	static void DeleteValue(const Slice& key, void* value) {
+		uint64_t* v = reinterpret_cast<uint64_t*>(value);
+		delete v;
+	}
+
+
+	class KeyUpdLru {
+	private:
+		std::shared_ptr<Cache> sstid_cache_;
+	public:
+		KeyUpdLru(const int kCacheSize) : sstid_cache_(NewLRUCache(kCacheSize)) {
+
+		}
+		~KeyUpdLru() {}
+
+		// add <key, SST_id> pair to updmap
+		// key is userkey
+		// if key already exists, the old SST_id score+1
+		void Add(Slice& key, uint64_t SST_id, ScoreTable* score_tbl) {
+			uint64_t* v_ptr = new uint64_t(SST_id);
+			Cache::Handle* handle = sstid_cache_->Lookup(key);
+			if (handle != nullptr) { //find key, so add score
+				uint64_t* id_ptr = reinterpret_cast<uint64_t*>(sstid_cache_->Value(handle));
+				//old id score +1
+				score_tbl->AddScore(*id_ptr);
+				sstid_cache_->Release(handle);
+			}
+			sstid_cache_->Insert(key, EncodeValue(v_ptr), 1, DeleteValue);
+		}
+
+		bool CompareAndUpdateSst(Slice& key, uint64_t old_id, uint64_t new_id) {
+			bool successful = false;
+			Cache::Handle* handle = sstid_cache_->Lookup(key);
+			if (handle != nullptr) { //find key
+				uint64_t* id_ptr = reinterpret_cast<uint64_t*>(sstid_cache_->Value(handle));
+				if (old_id == *id_ptr) {
+					sstid_cache_->Erase(key);
+					uint64_t* v_ptr = new uint64_t(new_id);
+					auto status = sstid_cache_->Insert(key, EncodeValue(v_ptr), 1, DeleteValue);
+					successful = status.ok();
+				}
+				sstid_cache_->Release(handle);
+			}
+			return successful;
+		}
+
+		bool FindSst(const Slice& key, uint64_t* value) {
+
+			Cache::Handle* handle = sstid_cache_->Lookup(key);
+			if (handle != nullptr) { //find key
+				uint64_t* id_ptr = reinterpret_cast<uint64_t*>(sstid_cache_->Value(handle));
+				*value = *id_ptr;
+				sstid_cache_->Release(handle);
+				return true;
+			}
+
+			return false;
+		}
+
+	};
+	
+}
+
+
+#endif
---
+++ include/rocksdb/sst_score_table.h
@@ -0,0 +1,77 @@
+#ifndef STORAGE_LEVELDB_DB_SSTSCORETABLE_H_
+#define STORAGE_LEVELDB_DB_SSTSCORETABLE_H_
+
+#include <unordered_map>
+#include <cassert>
+#include <mutex>
+#include <shared_mutex>
+
+namespace ROCKSDB_NAMESPACE {
+
+	struct ScoreSst {
+		int score;
+		uint64_t sst_id;
+	};
+
+	class ScoreTable {
+		public:
+
+			explicit ScoreTable(){
+				cur_highest.score = 0;
+				cur_highest.sst_id = 0;
+			}
+
+			void AddScore(uint64_t sst_id) {
+                std::unique_lock<std::shared_mutex> lock(mutex_);
+				auto it = score_table.find(sst_id);
+				if(it != score_table.end()) {
+					score_table[sst_id] = it->second + 1;
+				} else {
+					score_table[sst_id] = 1;
+				}
+				if (score_table[sst_id] > cur_highest.score) {
+					cur_highest.sst_id = sst_id;
+					cur_highest.score = score_table[sst_id];
+				}
+			}
+
+			void RemoveSstScore(uint64_t sst_id) {
+                std::unique_lock<std::shared_mutex> lock(mutex_);
+				score_table.erase(sst_id);
+			}
+
+			void ResetHighest() {
+                std::unique_lock<std::shared_mutex> lock(mutex_);
+				cur_highest.score = 0;
+				for (auto it: score_table) {
+					if (it.second > cur_highest.score) {
+						cur_highest.score = it.second;
+						cur_highest.sst_id = it.first;
+					}
+				}
+			}
+
+			bool Find(uint64_t sst_id) {
+                std::shared_lock<std::shared_mutex> lock(mutex_);
+				return score_table.find(sst_id) != score_table.end();
+			}
+
+			ScoreSst GetHighScoreSst() {
+                std::shared_lock<std::shared_mutex> lock(mutex_);
+				return cur_highest;
+			}
+
+			~ScoreTable(){}
+
+		private:
+            std::shared_mutex mutex_;
+			std::unordered_map<uint64_t, int> score_table;
+			ScoreSst cur_highest;
+            
+
+			
+	};
+
+}
+
+#endif
